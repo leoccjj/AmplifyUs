43f9fee8bc4fff8ba273ee50a6c2433392419f5b
Dimitri Diakopoulos <ddiakopoulos@gmail.com>
2014-04-23 14:18:36 -0700

    configuration files for sass, node, and grunt

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  require('time-grunt')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+
+    watch: {
+      assemble: {
+        files: ['{content,data,templates,sass}/{,*/}*.{md,hbs,yml,scss}'],
+        tasks: ['sass']
+      }
+    },
+
+    connect: {
+      options: {
+        port: 9000,        // clperge this to '0.0.0.0' to access the server from outside
+        hostname: 'localhost'
+      }
+    },
+
+    sass: {                              // Task
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'expanded',
+          compass: true, 
+        },
+        files: {
+          'stylesheets/screen.css': 'sass/screen.scss',       // 'destination': 'source'
+        }
+      }
+    }, 
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  grunt.registerTask('server', [
+    'connect:livereload',
+    'watch'
+  ]);
+
+  grunt.registerTask('build', [
+    'sass', 
+  ]);
+
+  grunt.registerTask('default', [
+    'build'
+  ]);
+
+};
